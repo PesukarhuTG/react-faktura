c74b260fcf33d36c5d1c77d16f070d012840541e
PesukarhuTG <ganzhinovat@yandex.ru>
2024-01-13 19:33:37 +0300

    refactor: delete component Copyrights, create as element in Footer

--- src/components/Copyrights/Copyrights.jsx
+++
@@ -1,5 +0,0 @@
-import style from "./Copyrights.module.scss";
-const Copyrights = () => (
-  <p className={style.copyright}>&copy; Фактура, 2024</p>
-);
-export default Copyrights;
--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -2,7 +2,6 @@ import Logo from "../../components/Logo/Logo";
 import Container from "../Container/Container";
 import Contacts from "../../components/Contacts/Contacts";
 import Developers from "../../components/Developers/Developers";
-import Copyrights from "../../components/Copyrights/Copyrights";
 import style from "./Footer.module.scss";
 
 const Footer = () => (
@@ -11,7 +10,7 @@ const Footer = () => (
       <Logo className={style.linkLogoFooter} />
       <Contacts />
       <Developers />
-      <Copyrights />
+      <p className={style.copyright}>&copy; Фактура, 2024</p>
     </Container>
   </footer>
 );
