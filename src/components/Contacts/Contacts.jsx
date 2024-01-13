6ded99baec388e101d4d87e538eb03d9cc37935c
PesukarhuTG <ganzhinovat@yandex.ru>
2024-01-13 21:58:49 +0300

    feat: add SVG icons for Contacts in footer

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,28 +1,33 @@
 import style from "./Contacts.module.scss";
 import cn from "classnames";
+import CallSVG from "/src/assets/icons/icon-call.svg?react";
+import VkontakteSVG from "/src/assets/icons/icon-vk.svg?react";
+import YoutubeSVG from "/src/assets/icons/icon-youtube.svg?react";
+import TelegramSVG from "/src/assets/icons/icon-tg.svg?react";
 
 const Contacts = () => (
   <div className={style.contacts}>
     <a className={cn(style.phone, style.link)} href="tel:+79398391297">
+      <CallSVG />
       <span>+7 (939) 839 12 97</span>
     </a>
 
     <ul className={style.list}>
       <li>
         <a className={style.link} href="#">
-          vk
+          <VkontakteSVG />
         </a>
       </li>
 
       <li>
         <a className={style.link} href="#">
-          yt
+          <YoutubeSVG />
         </a>
       </li>
 
       <li>
         <a className={style.link} href="#">
-          tg
+          <TelegramSVG />
         </a>
       </li>
     </ul>
